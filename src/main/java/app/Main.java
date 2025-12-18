820351bb19adc2edef9f15ee80a34e7faf0ecfb5
KIRAZINA <zinman323@gmail.com>
2025-12-18 17:35:31 +0200

    Initial commit: basic converter functionality

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+public class Main {
+
+    private static final double KG_TO_POUNDS = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Simple Converter App");
+
+        double kilograms = 5;
+
+        double resultPounds = convertKgToPounds(kilograms);
+
+        System.out.println(kilograms + " kg -> " + resultPounds + " lbs");
+    }
+
+    private static double convertKgToPounds(double kg) {
+        return kg * KG_TO_POUNDS;
+    }
+
+}
